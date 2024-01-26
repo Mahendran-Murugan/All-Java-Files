4f26836018ced3b3127dde32f701822c1b27c241
mahi000007 <mahendran1482004@gmail.com>
2024-01-26 22:05:01 +0530

    feat: add operators Sum of Two Numbers Soln

---
+++ SumOfTwoNo.java
@@ -0,0 +1,13 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class SumOfTwoNo {
+    public static void main(String[] args){
+        int n1 = 0, n2 = 0;
+        Scanner in = new Scanner(System. in) ;
+        n1 = in.nextInt();
+        n2 = in.nextInt();
+        System.out.printf ("'Sum: %d",n1+n2);
+    }
+}
