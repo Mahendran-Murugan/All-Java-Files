472c59dc16b8c085dd3096a3f8d7dea6ade61b57
mahi000007 <mahendran1482004@gmail.com>
2024-01-26 22:29:29 +0530

    feat: add Cricket Stadium Soln

---
+++ CricketStadium.java
@@ -0,0 +1,13 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class CricketStadium {
+    public static void main (String[] args) {
+        int length = 0, breadth = 0;
+        Scanner in = new Scanner(System.in);
+        length = in.nextInt();
+        breadth = in.nextInt();
+        System.out.printf("%d %d", (2 * (length + breadth)), (length * breadth));
+    }
+}
