ed1dca23f8b098826b90b0c9cf0f7f6381bd50cf
mahi000007 <mahendran1482004@gmail.com>
2024-01-26 22:18:04 +0530

    fix: operators Chronicles of Narnia Soln problem fixed

--- ChroniclesOfNarnia.java
+++ ChroniclesOfNarnia.java
@@ -1,4 +1,16 @@
 package Coding;
 
+import java.util.Scanner;
+
 public class ChroniclesOfNarnia {
+    public static void main (String[] args) {
+        int result = 0, num = 0;
+        Scanner in = new Scanner(System.in);
+        num = in.nextInt();
+        while (num > 0) {
+            result += num % 10;
+            num /= 10;
+        }
+        System.out.println(result);
+    }
 }
