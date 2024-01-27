0c98fa4e2b0f343b55b86f18a4eb10bffb0fdb76
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-01-27 19:53:48 +0530

    feat: add competitivePattern4 Soln

---
+++ CompetitivePattern4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class CompetitivePattern4{
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    for(int i = 1; i <= n; i++){
+      for(int j = i; j >= 1; j--){
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+}
