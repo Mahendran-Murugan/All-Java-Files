ed504b77562c5dc0ffa110a2c9b762afb2d97b9a
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-01-27 19:53:57 +0530

    feat: add competitivePattern5 Soln

---
+++ CompetitivePattern5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class CompetitivePattern5{
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    for(int i= 1; i <= n; i++){
+      for(int j = i; j <= n; j++){
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+}
