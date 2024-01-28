f7bb14f6f19b56942cdd9d97cbc580b3a79abbb8
mahi000007 <mahendran1482004@gmail.com>
2024-01-28 20:59:52 +0530

    feat: add series 2 soln

---
+++ Series2.java
@@ -0,0 +1,16 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class Series2 {
+    public static void main (String[] args) {
+        int n, st = 6, stAd = 3;
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(st + " ");
+            st += stAd;
+            stAd += 2;
+        }
+    }
+}
