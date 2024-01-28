ba5e2d808bbaf87a968ecd1eeeacdb03e3882852
mahi000007 <mahendran1482004@gmail.com>
2024-01-28 12:42:06 +0530

    feat: add GamesWithShapes soln

---
+++ GamesWithShapes.java
@@ -0,0 +1,14 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class GamesWithShapes {
+    public static void main (String[] args) {
+        int radius = 0, length = 0;
+        Scanner in = new Scanner(System.in);
+        radius = in.nextInt();
+        length = in.nextInt();
+        if (length >= (2 * radius)) System.out.println ("circle can be inside a square");
+        else System.out.println("circle cannot be inside a Square");
+    }
+}
