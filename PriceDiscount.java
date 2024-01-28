0e363db3dc8ec2fe28ae7a57643ecc11c99146a9
mahi000007 <mahendran1482004@gmail.com>
2024-01-28 20:55:25 +0530

    feat: add PriceDiscount soln

---
+++ PriceDiscount.java
@@ -0,0 +1,33 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class PriceDiscount {
+    public static void main (String[] args) {
+        int month;
+        double rate, disPrice = 0;
+        Scanner in = new Scanner(System. in) ;
+        rate = in.nextDouble();
+        month = in.nextInt();
+        switch (month){
+            case 1:
+                disPrice = rate * 0.20;
+                break;
+            case 3:
+                disPrice = rate * 0.10;
+                break;
+            case 6:
+            case 7:
+                disPrice = rate * 0.75;
+                break;
+            case 10:
+            case 11:
+                disPrice = rate * 0.30;
+                break;
+            case 12:
+                disPrice = rate * 0.35;
+                break;
+        }
+            System.out.printf("Chris father needs to pay Rs.%.2f", (rate - disPrice));
+    }
+}
