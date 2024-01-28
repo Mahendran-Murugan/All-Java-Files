dd6fb31da2d3b4f19473fce72db8e86a48e12205
mahi000007 <mahendran1482004@gmail.com>
2024-01-28 12:40:49 +0530

    feat: add SavingsWithExpenses Soln

---
+++ SavingsWithExpenses.java
@@ -0,0 +1,19 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class SavingsWithExpenses {
+    public static void main (String[] args) {
+        int salary = 0, interest = 0, hostelFee = 0, foodExp = 0, parentsCare = 0;
+        Scanner in = new Scanner(System.in);
+        salary = in.nextInt();
+        interest = in.nextInt();
+        hostelFee = in.nextInt();
+        foodExp = in.nextInt();
+        parentsCare = in.nextInt();
+        if (salary < (interest + hostelFee + foodExp + parentsCare))
+            System.out.println("He has to work hard");
+        else
+            System.out.println("He can save the money");
+    }
+}
