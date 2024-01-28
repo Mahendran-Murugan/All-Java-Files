0ba05600c83a5240c966fd46dda4e2bb99f490d2
mahi000007 <mahendran1482004@gmail.com>
2024-01-28 22:09:54 +0530

    feat: add series soln

---
+++ Series.java
@@ -0,0 +1,24 @@
+package Coding;
+
+import java.util.*;
+class Series{
+    public static void main (String[] args){
+        int n, count = 0;
+        boolean flag = true;
+        Scanner in = new Scanner (System. in) ;
+        n = in.nextInt();
+        for(int i = 101;count < n;i++){
+            flag = true;
+            for (int j = 2; j <= Math. sqrt (i); j++){
+                if(i%j==0){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                System.out.println(i);
+                count++;
+            }
+        }
+    }
+}
