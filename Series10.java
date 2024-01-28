7c29d0e13d64921d67b113342556514dcb1aee7d
mahi000007 <mahendran1482004@gmail.com>
2024-01-28 21:52:31 +0530

    feat: add series 10 soln

---
+++ Series10.java
@@ -0,0 +1,16 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class Series10 {
+    public static void main (String[] args) {
+        int n, st = 0, stAd = 2;
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print(st + " ");
+            st += stAd;
+            stAd += 2;
+        }
+    }
+}
