fbbc94ae242598cc52879d5dabea312f6639f174
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-01-30 22:08:27 +0530

    feat: add pattern10 sol

---
+++ Pattern10.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class Pattern10 {
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i = 1; i <= n; i++) {
+			for(int j = i; j >= 1; j--) {
+				System.out.print( j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
