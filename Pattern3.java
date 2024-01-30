361a8d9ad4eba806e7b9f0d81c03030521972ae0
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-01-30 22:07:33 +0530

    feat: add pattern3 sol

---
+++ Pattern3.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i =0; i < n; i++) {
+			for(int j = n-1; j > i; j--) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k <= i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
