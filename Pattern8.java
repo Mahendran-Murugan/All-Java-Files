39f846f983c5671399bdc13fc4fbdb6c79f00246
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-01-30 22:08:11 +0530

    feat: add pattern8 sol

---
+++ Pattern8.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class Pattern8 {
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i =0; i < n; i++) {
+			for(int j = 5; j > i; j--) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+}
