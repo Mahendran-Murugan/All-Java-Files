508c77e7d5d8c3410b4112b4f0dedf9ecaea8884
mahi000007 <mahendran1482004@gmail.com>
2024-01-31 06:40:50 +0530

    feat: add Decimal to Binary Soln

---
+++ DecimalToBinary.java
@@ -0,0 +1,19 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+
+    public static void toBinary(int n){
+        if(n > 0){
+            toBinary(n/2);
+            System.out.print(n%2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        toBinary(n);
+    }
+}
