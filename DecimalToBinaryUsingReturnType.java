d625f32df742310ac3f208919295f4986f6960ed
mahi000007 <mahendran1482004@gmail.com>
2024-02-04 20:40:15 +0530

    feat: add decimal to Binary conversion using return type in recursion

---
+++ DecimalToBinaryUsingReturnType.java
@@ -0,0 +1,17 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class DecimalToBinaryUsingReturnType {
+
+    public static int toBinary(int n){
+        if(n == 0) return 0;
+        return n % 2 + 10 * toBinary(n/2);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(toBinary(n));
+    }
+}
