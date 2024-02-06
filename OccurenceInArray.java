b54d77d0b1e51a8c35a3f6bd8db798604d2b9d88
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-02-06 16:04:29 +0530

    feat: add Occurence in Array sol

---
+++ OccurenceInArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class OccurenceInArray{
+  
+  public static void occurence(int n, int seEle, int[] arr, int count){
+    if(n < 0){
+      System.out.println(count);
+      return;
+    }
+    if(arr[n] == seEle) count++;
+    occurence(n-1, seEle, arr, count);
+  }
+  
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+      arr[i] = in.nextInt();
+    }
+    int x = in.nextInt();
+    occurence(n-1, x, arr, 0);
+  }
+}
