63b243dec6e7b8d50ea56d7e64b66ec5ddf439db
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-02-06 15:35:52 +0530

    feat: add multiplication table soln

---
+++ MultiplicationTableUsingRecursion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class MultiplicationTableUsingRecursion{
+
+  public static void mulTable(int n, int times){
+    if(times > 0){
+      mulTable(n, times-1);
+      System.out.printf("%d * %d = %d\n", n, times, n * times);
+    }
+  }
+  
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n =  in.nextInt();
+    mulTable(n, 10);
+  }
+}
