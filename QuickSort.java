4202e575f052eabb026ed81fe449fa4481318bc1
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-02-07 13:57:45 +0530

    feat: add Quick sort soln

---
+++ QuickSort.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class QuickSort {
+
+    public static void swap(int[] arr, int i, int pi){
+        int temp = arr[i];
+        arr[i] = arr[pi];
+        arr[pi] = temp;
+    }
+
+    public static int partition(int[] arr, int st, int end){
+        int pivot = arr[end];
+        int pivotIndex = -1;
+        for(int i = 0; i < end; i++){
+            if(arr[i] < pivot){
+                ++pivotIndex;
+                swap(arr, i, pivotIndex);
+            }
+        }
+        ++pivotIndex;
+        swap(arr, pivotIndex, end);
+        return pivotIndex;
+    }
+
+    public static void quickSort(int[] arr, int st, int end){
+        if(st < end){
+            int pivotPos = partition(arr, st, end);
+            quickSort(arr, st, pivotPos-1); 
+            quickSort(arr, pivotPos+1, end); 
+        }
+    }
+
+    public static void main(String[] args){
+        int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        quickSort(arr, 0, arr.length-1);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
