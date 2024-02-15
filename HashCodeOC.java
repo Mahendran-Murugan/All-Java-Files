b3f1c3b0a198e9f0d3773430a46008ae5b81afce
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-02-15 10:42:05 +0530

    feat: learn hash code method in Objectclass

---
+++ HashCodeOC.java
@@ -0,0 +1,11 @@
+
+class A{
+    int a, b;
+}
+
+public class HashCodeOC {
+    public static void main(String[] args) {
+        A a1 = new A();
+        System.out.println(a1.hashCode());
+    }
+}
