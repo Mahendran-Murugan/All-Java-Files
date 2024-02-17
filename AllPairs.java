70e273e878a33f500639a8e072ff3efb1eec89e5
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-02-17 14:38:30 +0530

    fix: fix code

--- AllPairs.java
+++ AllPairs.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
 
 class Pairs{
-  void read(int[] arr, int n){
-    Scanner in = new Scanner(System.in);
-    for(int i = 0; i < n; i++) arr[i] = in.nextInt();
-    int x = in.nextInt();
-    calc(arr, n, x);
-  }
-  void calc(int[] arr, int n, int x){
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < n; j++){
-          if(arr[i]+arr[j] == x){
-            System.out.println(arr[i]+ " " + arr[j]+" ");
-          }
-      }
-    }
+  int[] arr;
+  Pairs(int n){
+    this.arr = new int[n];
   }
 }
 
 public class AllPairs{
+
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
-    int arr[] = new int[n];
-    Pairs p = new Pairs();
-    p.read(arr, n);
+    Pairs p = new Pairs(n);
+    for(int i = 0; i < n; i++) p.arr[i] = in.nextInt();
+    int x = in.nextInt();
+
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        if(p.arr[i]+p.arr[j] == x){
+          System.out.println(p.arr[i]+ " " + p.arr[j]+" ");
+        }
+      }
+    }
   }
 }
