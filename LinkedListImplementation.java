1a4bb0ef5808b42d182d88bba89bdb74aea5511b
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-02-22 09:39:14 +0530

    feat: learn linked list implementation

---
+++ LinkedListImplementation.java
@@ -0,0 +1,41 @@
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class linkedList{
+    Node head;
+    linkedList(){
+        head = null;
+    }
+
+    void addFirst(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void print(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.print("null");
+    }
+}
+
+public class LinkedListImplementation {
+    public static void main(String[] args) {
+        linkedList ll = new linkedList();
+        ll.addFirst(10);
+        ll.addFirst(20);
+        ll.addFirst(30);
+        ll.print();
+    }
+}
