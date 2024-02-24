ba5b7e1e7fc0e872e3a2496e10b416699f1e631c
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-02-24 13:26:11 +0530

    feat: learn Treeset

---
+++ Collections/Set/TreeSetJava.java
@@ -0,0 +1,17 @@
+
+package Collections.Set;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class TreeSetJava {
+    public static void main(String[] args) {
+        Set s = new TreeSet();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            s.add(in.nextInt());
+        }
+        System.out.println(s);
+    }
+}
