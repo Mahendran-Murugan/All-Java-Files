c1800cfcb3da3259e8f1a4aec320e313049e7da6
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-02-24 13:33:38 +0530

    feat: learn PriorityQueue using Linked List

---
+++ Collections/Queue/PriorityQueueJava.java
@@ -0,0 +1,18 @@
+package Collections.Queue;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class PriorityQueueJava {
+    public static void main(String[] args) {
+        Queue q = new LinkedList();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0; i < n; i++){
+            q.add(in.nextInt());
+        }
+        for(int i = 0; i < n; i++){
+            System.out.println(q.poll());
+        }
+    }
+}
