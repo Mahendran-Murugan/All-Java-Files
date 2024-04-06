3bfc7a42b20038a02aede0bbd261818990136649
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-04-06 11:38:49 +0530

    feat: remove the arr in fun call:

--- RobotMove.java
+++ RobotMove.java
@@ -7,7 +7,7 @@ public class RobotMove {
             return 1;
         if (n < 0 || m < 0)
             return 0;
-        return possibleWays(arr, n - 1, m) + possibleWays(arr, n, m - 1);
+        return possibleWays(n - 1, m) + possibleWays(n, m - 1);
     }
 
     public static void main(String[] args) {
