f92ee399a28105ebe730d83c126238e251638bc0
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-04-08 11:44:03 +0530

    fix: use priority Queue

--- printIntegersInAccendingOrderQueue.java
+++ printIntegersInAccendingOrderQueue.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class printIntegersInAccendingOrderQueue {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Queue<Integer> q = new PriorityQueue<Integer>();
+        Queue<Character> q = new PriorityQueue<Character>();
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
-            q.add(in.nextInt());
+            q.add(in.next().charAt(0));
         }
         while(!q.isEmpty()){
             System.out.print(q.poll()+" ");
