3ee3b12a16682d8907f7d60f051223683ec69482
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-04-08 10:20:14 +0530

    feat: add priority queue and understand differnt methods

--- Collections/Queue/PriorityQueueJava.java
+++ Collections/Queue/PriorityQueueJava.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 public class PriorityQueueJava {
     public static void main(String[] args) {
-        Queue<Integer> q = new PriorityQueue<Integer>();
+        Queue<Character> q = new PriorityQueue<Character>();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
-            q.add(in.nextInt());
+            q.add(in.next().charAt(0));
         }
+        
         System.out.println(q);
         in.close();
     }
