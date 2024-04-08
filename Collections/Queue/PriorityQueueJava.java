5781f707948bebcb1d9aca5c51494d697520206e
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-04-08 09:47:13 +0530

    feat: add changes in priorityqueue program

--- Collections/Queue/PriorityQueueJava.java
+++ Collections/Queue/PriorityQueueJava.java
@@ -4,14 +4,13 @@ import java.util.*;
 
 public class PriorityQueueJava {
     public static void main(String[] args) {
-        Queue q = new PriorityQueue();
+        Queue<Integer> q = new PriorityQueue<Integer>();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         for(int i = 0; i < n; i++){
             q.add(in.nextInt());
         }
-        for(int i = 0; i < n; i++){
-            System.out.println(q.poll());
-        }
+        System.out.println(q);
+        in.close();
     }
 }
