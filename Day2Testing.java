b9d26daf9e9a4d4f4102a74198de25811d1fb600
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-06-11 18:25:49 +0530

    feat: recall input stream

---
+++ Day2Testing.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day2Testing {
+    public static void main(String[] args) throws IOException {
+        // int x = 100;
+        // System.out.printf("Printing simple" + " integer: x = %d\n", x);
+        // System.out.printf("Formatted with" + " precision: PI = %.3f\n", Math.PI);
+        // float n = 5.2f;
+        // System.out.printf("Formatted to " + "specific width: n = %.4f\n", n);
+        // n = 2324435.3f;
+        // System.err.printf("Formatted to " + "right margin: n = %13.4f\n", n);
+
+        // InputStreamReader in = new InputStreamReader(System.in);
+        // int ch;
+        // while ((ch = in.read()) != '0') {
+        // System.out.println((char) ch);
+        // }
+    }
+}
