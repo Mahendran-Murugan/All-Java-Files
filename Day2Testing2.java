d36c87778f008bf4e27d114effb6255e22c23d7a
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-06-11 19:30:30 +0530

    feat: recall abstarction

---
+++ Day2Testing2.java
@@ -0,0 +1,54 @@
+abstract class fruit {
+    abstract void fruitInfo();
+
+    void typeOfFood() {
+        System.out.println("Fruit");
+    }
+}
+
+class Mango extends fruit {
+    @Override
+    void fruitInfo() {
+        System.out.println("Mango");
+    }
+}
+
+interface Pet {
+    abstract void eat();
+
+    abstract void run();
+
+    abstract void sleep();
+
+    default void creatureType() {
+        System.out.println("Pet");
+    }
+}
+
+class Doggy implements Pet {
+    @Override
+    public void eat() {
+        System.out.println("Dog eats");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Dog run");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Dog sleep");
+    }
+}
+
+public class Day2Testing2 extends Doggy {
+    public static void main(String[] args) {
+        fruit day2 = new Mango();
+        day2.fruitInfo();
+        day2.typeOfFood();
+        System.out.println(day2 instanceof Mango);
+        Day2Testing2 d = new Day2Testing2();
+        d.creatureType();
+    }
+}
