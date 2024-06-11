edac3710cc39f9b2855244abce09793f6c8c9302
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-06-11 18:59:26 +0530

    feat: recall input and output streams in java

--- Day2Testing.java
+++ Day2Testing.java
@@ -1,5 +1,4 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Day2Testing {
     public static void main(String[] args) throws IOException {
@@ -16,5 +15,39 @@ public class Day2Testing {
         // while ((ch = in.read()) != '0') {
         // System.out.println((char) ch);
         // }
+
+        // String m = "Mahendran";
+        // byte[] b = m.getBytes();
+        // for (byte i : b) {
+        // System.out.println((char) i);
+        // }
+        // FileOutputStream fout = new FileOutputStream(new File("Tester.txt"));
+        // String str = "Mahendran is Learning Java";
+        // byte[] b = str.getBytes();
+        // fout.write(b);
+        // System.out.println("Writing Successfull");
+        // fout.close();
+
+        // FileInputStream fin = new FileInputStream(new File("Tester.txt"));
+        // int b;
+        // while ((b = fin.read()) != -1) {
+        // System.out.print((char) b);
+        // }
+        // fin.close();
+
+        // BufferedReader reader = new BufferedReader(new FileReader(new
+        // File("Tester.txt")));
+        // String str;
+        // while ((str = reader.readLine()) != null) {
+        // System.out.print(str);
+        // }
+        // reader.close();
+
+        // BufferedWriter writer = new BufferedWriter(new FileWriter(new
+        // File("Tester.txt")));
+        // String s = "mahendran is awsome";
+        // writer.write(s);
+        // writer.close();
+
     }
 }
