24deb37f0cd00d86fd7723931bc9fd2d4f3f8e46
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-06-13 18:53:54 +0530

    feat: recall singly linked list in java

--- LinkedList.java
+++ LinkedList.java
@@ -1,73 +1,80 @@
 import java.util.Scanner;
 
-class Node{
+class Node {
     int data;
     Node next;
-    Node(int data){
+
+    Node(int data) {
         this.data = data;
         this.next = null;
     }
 }
-class List{
+
+// 1 -> 2
+class List {
     Node head, tail;
-    List(){
+
+    List() {
         head = tail = null;
     }
-    void insertAtBeggining(int data){
+
+    void insertAtBeggining(int data) {
         Node newNode = new Node(data);
-        if(head == null){
+        if (head == null) {
             tail = newNode;
         }
         newNode.next = head;
         head = newNode;
         System.out.println("Element is inserted..");
     }
-    void insertAtEnd(int data){
-        if(tail == null){
+
+    void insertAtEnd(int data) {
+        if (tail == null) {
             insertAtBeggining(data);
-        }else{
+        } else {
             Node newNode = new Node(data);
             tail.next = newNode;
             tail = newNode;
         }
         System.out.println("Element is inserted..");
     }
-    void traverse(){
+
+    void traverse() {
         System.out.println("The List is: ");
         Node temp = head;
-        while(temp != null){
-            System.out.print(temp.data+"->");
+        while (temp != null) {
+            System.out.print(temp.data + "->");
             temp = temp.next;
         }
-        System.out.print("null\n"); 
+        System.out.print("null\n");
     }
-    void deleteAtBeggining(){
-        if(head == null){
+
+    void deleteAtBeggining() {
+        if (head == null) {
             System.out.println("List is Empty");
-        }else{
-            if(head.next == null) head = null;
-            else{
-                head = head.next;
-            }
+        } else {
+            head = head.next;
             System.out.println("Element is deleted..");
         }
     }
-    void deleteAtEnd(){
-        if(head == null){
+
+    void deleteAtEnd() {
+        if (head == null) {
             System.out.println("List is Empty");
-        }else{
-            if(head.next == null) head = null;
-        else{
-            Node temp = head;
-            while(temp.next.next != null){
-                temp = temp.next;
-            }
-            tail = temp;
-            temp.next = null;
+        } else {
+            if (head.next == null)
+                head = null;
+            else {
+                Node temp = head;
+                while (temp.next.next != null) {
+                    temp = temp.next;
+                }
+                tail = temp;
+                temp.next = null;
             }
             System.out.println("Element is deleted..");
         }
-        
+
     }
 }
 
@@ -77,14 +84,15 @@ public class LinkedList {
         int data;
         List l = new List();
         while (true) {
-            System.out.print("Select your Choice:\n1.Insert at Beggining\n2.Insert at End\n3.Traverse\n4.Delete at Beggining\n5.Delete at End\n6.Exit\nEnter your option:");
+            System.out.print(
+                    "Select your Choice:\n1.Insert at Beggining\n2.Insert at End\n3.Traverse\n4.Delete at Beggining\n5.Delete at End\n6.Exit\nEnter your option:");
             int ch = in.nextInt();
             switch (ch) {
                 case 1:
                     data = in.nextInt();
                     l.insertAtBeggining(data);
                     break;
-                    
+
                 case 2:
                     data = in.nextInt();
                     l.insertAtEnd(data);
@@ -101,7 +109,7 @@ public class LinkedList {
                 case 6:
                     System.exit(1);
                     break;
-            
+
                 default:
                     break;
             }
